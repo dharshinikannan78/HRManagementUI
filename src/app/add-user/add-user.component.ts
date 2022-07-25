90fad847f65cb37d9629ef7faf7c590bb2f14c62
salmank07 <mohamedsalmankhan509@gmail.com>
2022-07-25 15:27:34 +0530

    inicommit

---
+++ src/app/add-user/add-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.scss']
+})
+export class AddUserComponent implements OnInit {
+  loading: boolean = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
